bba57e0eec9d40216f55d0e20f4957acaeb9aacd
mhasaero <147121238+mhasaero@users.noreply.github.com>
2023-10-10 11:46:14 +0700

    Delete tugas PTI/alert directory

--- tugas PTI/alert/script.js
+++
@@ -1,7 +0,0 @@
-const footerArticle = document.querySelector("footer-article");
-
-footerArticle.addEventListener("click", function (e) {
-  if (e.target.classList == "love") {
-    e.target.style.path = "black";
-  }
-});
